c547ef313db2fbaca09faa033edbbb6d59c51252
GyuSikYoon <gyumiot@gmail.com>
2026-04-16 00:18:24 +0900

    fix:ChatSocketController 수정

--- backend/src/main/java/com/devnear/web/controller/chat/ChatSocketController.java
+++ backend/src/main/java/com/devnear/web/controller/chat/ChatSocketController.java
@@ -4,8 +4,10 @@ import com.devnear.web.domain.user.User;
 import com.devnear.web.dto.chat.ChatMessageResponse;
 import com.devnear.web.dto.chat.ChatMessageSendRequest;
 import com.devnear.web.service.chat.ChatService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
@@ -18,7 +20,7 @@ public class ChatSocketController {
     private final SimpMessagingTemplate messagingTemplate;
 
     @MessageMapping("/chat/send")
-    public void sendMessage(ChatMessageSendRequest request, Authentication authentication) {
+    public void sendMessage(@Valid @Payload ChatMessageSendRequest request, Authentication authentication) {
         // 웹소켓 인증 객체에서 현재 로그인 유저 꺼내기
         User user = (User) authentication.getPrincipal();
 
