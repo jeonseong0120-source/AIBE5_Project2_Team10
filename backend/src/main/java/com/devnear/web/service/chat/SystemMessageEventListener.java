b14633872393b9c82b243c8e563e412c37540f1f
warcat12 <warcat12@gmail.com>
2026-04-16 17:34:31 +0900

    fix/ #42
    코드 누락 수정

--- backend/src/main/java/com/devnear/web/service/chat/SystemMessageEventListener.java
+++ backend/src/main/java/com/devnear/web/service/chat/SystemMessageEventListener.java
@@ -1,15 +1,16 @@
 package com.devnear.web.service.chat;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class SystemMessageEventListener {
-
     private final SimpMessagingTemplate messagingTemplate;
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
@@ -19,6 +20,5 @@ public class SystemMessageEventListener {
         } catch (RuntimeException ex) {
             log.error("Failed to broadcast system message. roomId={}", event.roomId(), ex);
         }
-
     }
 }
