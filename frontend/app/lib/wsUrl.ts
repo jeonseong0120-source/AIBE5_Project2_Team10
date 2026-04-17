9e9d8540a946dcb20334d919051eeeec502fd494
warcat12 <warcat12@gmail.com>
2026-04-17 10:05:55 +0900

    fix/ #37 코드래빗 반영

--- frontend/app/lib/wsUrl.ts
+++ frontend/app/lib/wsUrl.ts
@@ -3,6 +3,9 @@ import { resolveApiBaseUrl } from "./axios";
 /** STOMP SockJS 엔드포인트는 `/api` 접두사 밖에 둡니다. */
 export function resolveSockJsUrl(): string {
     const api = resolveApiBaseUrl();
-    const origin = api.replace(/\/api\/?$/i, "");
-    return `${origin}/ws-chat`;
+    const url = new URL(api);
+    url.search = "";
+    url.hash = "";
+    const basePath = url.pathname.replace(/\/api\/?$/i, "").replace(/\/+$/, "");
+    return `${url.origin}${basePath}/ws-chat`;
 }
