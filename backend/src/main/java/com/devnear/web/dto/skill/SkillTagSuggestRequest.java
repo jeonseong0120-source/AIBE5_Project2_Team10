27346943071d9ac32a51b4d69f825c9b6eca959d
jeonseong0120 <jeonseong922@gmail.com>
2026-04-21 11:34:20 +0900

    bug : 세미콜론 삭제

--- backend/src/main/java/com/devnear/web/dto/skill/SkillTagSuggestRequest.java
+++ backend/src/main/java/com/devnear/web/dto/skill/SkillTagSuggestRequest.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Size;
 
 @Getter
